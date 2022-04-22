7541a2f6a6c74c144c91d556155e8fab742510bf
lohit valavala <61038912+vlkumar-859@users.noreply.github.com>
2022-04-22 18:32:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import Products from './Products';
+
+const App = () => {
+  const [search,setSearch] = useState('');
+  const [data,setData] = useState([]);
+  const YOUR_APP_ID = "e727b94b";
+  const YOUR_APP_KEY ="b00acf157b0516e9de6ec43b8d92dfb7";
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
+      response => response.json()
+    ).then(
+       data => setData(data.hits) 
+    )
+  }
+  return (
+    <div>
+      <center>
+        <h4>Nutri Tracker</h4>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
+          <input type="submit" className="btn btn-primary" value="Search"/>
+        </form>
+        {data.length>=1 ? <Products  data={data}/>:null}
+      </center>
+    </div>
+  )
+}
+
+export default App
